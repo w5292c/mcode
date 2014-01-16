cb516d5f82d3d443c9c0a9f19e8794e9ba8860d1
Alexander Chumakov <alexander.chumakov@symphonyteleca.com>
2014-01-16 10:13:36 +0400

    Added AVR and PC (emulator) targets

---
+++ src/main-avr.c
@@ -0,0 +1,16 @@
+#include "cmd-engine.h"
+
+int main (void)
+{
+  cmd_engine_init ();
+
+  /* main dummy loop */
+  while (1)
+  {
+    /* 0.1s = 100ms = 100000 us */
+    usleep(100000);
+  }
+
+  cmd_engine_deinit ();
+  return 0;
+}
