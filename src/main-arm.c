a6f3ebaa355be7959c8dc0321c3f44c4dbb031d7
Alexander Chumakov <w5292c@gmail.com>
2015-01-04 10:34:23 +0300

    First working variant with stubs

--- src/main-arm.c
+++ src/main-arm.c
@@ -24,17 +24,56 @@
 
 #include "scheduler.h"
 
+#include <stm32f10x.h>
+
 int main (void)
 {
+  /* GPIO configuration */
+  GPIO_InitTypeDef GPIO_Config;
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+  GPIO_Config.GPIO_Pin =  GPIO_Pin_5;
+  GPIO_Config.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_Config.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOB, &GPIO_Config);
+  GPIO_Config.GPIO_Pin =  GPIO_Pin_8;
+  GPIO_Init(GPIOB, &GPIO_Config);
+  GPIO_Config.GPIO_Pin =  GPIO_Pin_9;
+  GPIO_Init(GPIOB, &GPIO_Config);
+  GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_SET);
+
   /* first, init the scheduler */
   mcode_scheduler_init ();
 
+#if 0
   mcode_scheduler_start ();
-  return 0;
-}
+#else
+  /* Implement something visible, to be removed later, as soon as implemented properly */
+  int s = 0;
+  for (;;) {
+    switch(s) {
+    case 0:
+      GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_SET);
+      GPIO_WriteBit(GPIOB, GPIO_Pin_8, Bit_RESET);
+      GPIO_WriteBit(GPIOB, GPIO_Pin_9, Bit_RESET);
+      break;
+    case 1:
+      GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_RESET);
+      GPIO_WriteBit(GPIOB, GPIO_Pin_8, Bit_SET);
+      GPIO_WriteBit(GPIOB, GPIO_Pin_9, Bit_RESET);
+      break;
+    case 2:
+      GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_RESET);
+      GPIO_WriteBit(GPIOB, GPIO_Pin_8, Bit_RESET);
+      GPIO_WriteBit(GPIOB, GPIO_Pin_9, Bit_SET);
+      break;
+    default:
+      break;
+    }
 
-void _exit(int status)
-{
-  (void)status;
-  for(;;);
+    for (unsigned int i=0;i<0xfffffU;i++);
+    if (++s > 2) s = 0;
+  }
+#endif
+
+  return 0;
 }
