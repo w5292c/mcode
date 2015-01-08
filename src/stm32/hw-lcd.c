5855bedcc8a44fef465db807981756dd54ec5d0b
Alexander Chumakov <w5292c@gmail.com>
2015-01-08 14:52:31 +0300

    Added preliminary files for STM32 LCD support

---
+++ src/stm32/hw-lcd.c
@@ -0,0 +1,52 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Alexander Chumakov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "hw-lcd.h"
+
+static lcd_read_cb TheReadCallback = 0;
+
+void lcd_init(void)
+{
+}
+
+void lcd_deinit(void)
+{
+}
+
+void lcd_reset(void)
+{
+}
+
+void lcd_set_read_cb(lcd_read_cb cb)
+{
+  TheReadCallback = cb;
+}
+
+void lcd_read(uint8_t cmd, uint8_t length)
+{
+}
+
+void lcd_write(uint8_t cmd, uint8_t length, const uint8_t *data)
+{
+}
