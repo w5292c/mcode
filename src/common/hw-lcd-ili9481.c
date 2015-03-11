a06df1cc8f8e3e6dfdf83d92cf638ae1d8c8e979
Alexander Chumakov <w5292c@gmail.com>
2015-03-11 10:11:52 +0300

    Updated common code for ILI9481 based LCDes

--- src/common/hw-lcd-ili9481.c
+++ src/common/hw-lcd-ili9481.c
@@ -24,33 +24,41 @@
 
 #include "hw-lcd.h"
 
+uint16_t lcd_get_width(void)
+{
+  return UINT16_C(320);
+}
+
+uint16_t lcd_get_height(void)
+{
+  return UINT16_C(480);
+}
+
 void lcd_turn(bool on)
 {
-  if (on) {
-    /* exit_sleep_mode */
-    lcd_command(0x11);
-    lcd_command(0x50);
-    /* Power setting */
-    lcd_command(0xD0, 0x07, 0x42, 0x18);
-    /* VCOM */
-    lcd_command(0xD1, 0x00, 0x07, 0x10);
-    /* Power setting for norm. mode */
-    lcd_command(0xD2, 0x01, 0x02);
-    /* Panel driving setting */
-    lcd_command(0xC0, 0x10, 0x3B, 0x00, 0x02, 0x11);
-    /* Frame rate & inv. */
-    lcd_command(0xC5, 0x03);
-    /* set_pixel_format: 16bpp */
-    lcd_command(0x3A, 0x55);
-    lcd_command(0x36, 0x09);
-    /* @todo Implement gamma: 0xC8, 0x00, 0x32, 0x36, 0x45, 0x06, 0x16, 0x37, 0x75, 0x77, 0x54, 0x0C, 0x00, */
-    /* set_display_on */
-    lcd_command(0x29);
-    lcd_set_scroll_start(UINT16_C(0));
-  } else {
-    /* set_display_off */
-    lcd_command(0x28);
-    /* enter_sleep_mode */
-    lcd_command(0x10);
-  }
+  lcd_command(on ? UINT8_C(0x29) : UINT8_C(0x28));
+}
+
+void lcd_device_init(void)
+{
+  /* exit_sleep_mode */
+  lcd_command(0x11);
+  lcd_command(0x50);
+  /* Power setting */
+  lcd_command(0xD0, 0x07, 0x42, 0x18);
+  /* VCOM */
+  lcd_command(0xD1, 0x00, 0x07, 0x10);
+  /* Power setting for norm. mode */
+  lcd_command(0xD2, 0x01, 0x02);
+  /* Panel driving setting */
+  lcd_command(0xC0, 0x10, 0x3B, 0x00, 0x02, 0x11);
+  /* Frame rate & inv. */
+  lcd_command(0xC5, 0x03);
+  /* set_pixel_format: 16bpp */
+  lcd_command(0x3A, 0x55);
+  lcd_command(0x36, 0x09);
+  /* @todo Implement gamma: 0xC8, 0x00, 0x32, 0x36, 0x45, 0x06, 0x16, 0x37, 0x75, 0x77, 0x54, 0x0C, 0x00, */
+  /* set_display_on */
+  lcd_command(0x29);
+  lcd_set_scroll_start(UINT16_C(0));
 }
