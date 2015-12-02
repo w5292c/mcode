27cd70818de6a4638a755b611b80593c0d9fc8ef
Alexander Chumakov <w5292c@gmail.com>
2015-12-02 11:27:44 +0300

    Fixed compilation error in SSL source files

--- src/openssl/sha256.c
+++ src/openssl/sha256.c
@@ -125,7 +125,7 @@ static
 #endif
 void sha256_block_data_order(SHA256_CTX *ctx, const void *in, size_t num);
 
-#include "internal/md32_common.h"
+#include "md32_common.h"
 
 #ifndef SHA256_ASM
 static const SHA_LONG K256[64] = {
