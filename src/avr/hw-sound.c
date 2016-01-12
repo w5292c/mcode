2e3080b02d51fcad48b4da9422ac693a7f160c8d
Alexander Chumakov <w5292c@gmail.com>
2016-01-12 14:10:44 +0300

    Added preliminary hw-sound support on Timer2

---
+++ src/avr/hw-sound.c
@@ -0,0 +1,132 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Alexander Chumakov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "hw-sound.h"
+
+#include "mtick.h"
+#include "strings.h"
+
+#include <stdbool.h>
+#include <avr/pgmspace.h>
+
+/* 2*Fclk/Fnote: 4 times (OCRn plus one), when N (prescaler) is one; */
+static const uint16_t TheBase[] PROGMEM = {
+  56361u, /*< C  : 261.63 Hz */
+  53199u, /*< C# : 277.18 Hz */
+  50213u, /*< D  : 293.66 Hz */
+  47394u, /*< D# : 311.13 Hz */
+  44734u, /*< E  : 329.63 Hz */
+  42223u, /*< F  : 349.23 Hz */
+  39854u, /*< F# : 369.99 Hz */
+  37616u, /*< G  : 392.00 Hz */
+  35506u, /*< G# : 415.30 Hz */
+  33513u, /*< A  : 440.00 Hz */
+  31632u, /*< B  : 466.16 Hz */
+  29857u  /*< H  : 493.88 Hz */
+};
+
+static const uint16_t ThePreScalers[] PROGMEM = {
+  1u, 8u, 32u, 64u, 128u, 256u, 1024u
+};
+
+void hw_sound_init(void)
+{
+}
+
+void hw_sound_deinit(void)
+{
+}
+
+void sound_play_note(uint8_t note, uint16_t length)
+{
+  while (true) {
+    /* Parse the tone of the note */
+    const uint8_t tone = (note & 0x0fu);
+    if (tone > 0x0bu) {
+      break;
+    }
+
+    /* Parse the octave of the note */
+    uint8_t octave = ((note>>4) & 0x0fu) - 4;
+    if (octave > 4) {
+      break;
+    }
+
+    uint16_t base = pgm_read_word(&TheBase[tone]);
+    while (octave--) {
+      base >>= 1;
+    }
+
+    uint8_t i;
+    uint16_t prescaler;
+    uint16_t countPlusOne;
+    for (i = 0; i < sizeof (ThePreScalers)/sizeof (*ThePreScalers); ++i) {
+      prescaler = pgm_read_word(&ThePreScalers[i]);
+      countPlusOne = (base/prescaler + 2)>>2;
+      if (countPlusOne > 0u && countPlusOne < 0x100u) {
+        break;
+      }
+    }
+    if (countPlusOne < 1u || countPlusOne > 0xffu) {
+      break;
+    }
+
+    /* Configure the timer2 HW */
+    TCCR2 =
+      (1<<WGM21)|(0<<WGM20)| /*< Counter mode: CTC */
+      (0<<COM21)|(1<<COM20); /*< Output mode: Toggle output */
+    TCNT2 = 0;
+    OCR2 = (uint8_t)(countPlusOne - 1);
+    switch (prescaler) {
+    case 1:
+      TCCR2 |= (0<<CS22)|(0<<CS21)|(1<<CS20);
+      break;
+    case 8:
+      TCCR2 |= (0<<CS22)|(1<<CS21)|(0<<CS20);
+      break;
+    case 32:
+      TCCR2 |= (0<<CS22)|(1<<CS21)|(1<<CS20);
+      break;
+    case 64:
+      TCCR2 |= (1<<CS22)|(0<<CS21)|(0<<CS20);
+      break;
+    case 128:
+      TCCR2 |= (1<<CS22)|(0<<CS21)|(1<<CS20);
+      break;
+    case 256:
+      TCCR2 |= (1<<CS22)|(1<<CS21)|(0<<CS20);
+      break;
+    case 1024:
+      TCCR2 |= (1<<CS22)|(1<<CS21)|(1<<CS20);
+      break;
+    default:
+      /* should not appear here, disable output (on default) */
+      merror(MStringInternalError);
+      break;
+    }
+  }
+
+  const uint64_t target = mtick_count() + length + 1;
+  while (target > mtick_count()) {}
+}
---
+++ src/hw-sound.h
@@ -0,0 +1,57 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Alexander Chumakov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef HW_MCODE_SOUND_H
+#define HW_MCODE_SOUND_H
+
+#include <stdint.h>
+
+void sound_init(void);
+void sound_deinit(void);
+
+/**
+ * Play a note:
+ * --------------------    ----------------------
+ * |  # | Note | Code |    |  # | Octave | Code |
+ * --------------------    ----------------------
+ * |  1 |   C  | 0x00 |    |  1 |     0  | ---- |
+ * |  2 |   C# | 0x01 |    |  2 |     1  | ---- |
+ * |  3 |   D  | 0x02 |    |  3 |     2  | ---- |
+ * |  4 |   D# | 0x03 |    |  4 |     3  | ---- |
+ * |  5 |   E  | 0x04 |    |  5 |    (4) | 0x40 |
+ * |  6 |   F  | 0x05 |    |  6 |     5  | 0x50 |
+ * |  7 |   F# | 0x06 |    |  7 |     6  | 0x60 |
+ * |  8 |   G  | 0x07 |    |  8 |     7  | 0x70 |
+ * |  9 |   G# | 0x08 |    |  9 |     8  | 0x80 |
+ * | 10 |  (A) | 0x09 |    ----------------------
+ * | 11 |   B  | 0x0A |
+ * | 12 |   H  | 0x0B |
+ * --------------------
+ * @param[in] note - The Note and Octave information
+ * @param[in] length - The length of the note in msec
+ * @note Special 'note' value 0xFFU means 'pause' for 'length' msec
+ */
+void sound_play_note(uint8_t note, uint16_t length);
+
+#endif /* HW_MCODE_SOUND_H */
