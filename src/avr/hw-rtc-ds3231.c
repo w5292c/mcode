64e9fa47eba662ab38fed6bdcd1f723a03643998
Alexander Chumakov <w5292c@gmail.com>
2016-01-19 11:26:41 +0300

    Updated RTC-code for AVR to use sync TWI API

--- src/avr/hw-rtc-ds3231.c
+++ src/avr/hw-rtc-ds3231.c
@@ -32,21 +32,10 @@
 
 #include <avr/interrupt.h>
 
-typedef enum {
-  RtcAlarmStateNull = 0,
-  RtcAlarmStateIdle,
-  RtcAlarmStateSetStatusAddress,
-  RtcAlarmStateReadStatus,
-  RtcAlarmStateClearFlag,
-} RtcAlarmState;
-
-static uint8_t TheState;
 static bool volatile TheIsrRequest;
 
 static void rtc_alarm_tick(void);
-static void rtc_alarm_check_status(uint8_t status);
-static void rtc_alarm_twi_write_done(bool success);
-static void rtc_alarm_read_ready(bool success, uint8_t length, const uint8_t *data);
+static bool rtc_alarm_check_status(uint8_t status);
 
 void rtc_alarm_init(void)
 {
@@ -58,7 +47,6 @@ void rtc_alarm_init(void)
   MCUCR |= (0U<<ISC10)|(0U<<ISC11);
   /* Enable external interrupt request INT1 (PD3) */
   GICR |= _BV(INT1);
-  TheState = RtcAlarmStateIdle;
   mcode_scheduler_add(rtc_alarm_tick);
 }
 
@@ -66,71 +54,38 @@ void rtc_alarm_deinit(void)
 {
 }
 
-static uint8_t TheBuffer[2];
 void rtc_alarm_tick(void)
 {
   if (TheIsrRequest) {
     /* Now, check which alarm expired */
-    TheBuffer[0] = 0x0f;
-    twi_send(0xd0, 1, TheBuffer, rtc_alarm_twi_write_done);
-    TheState = RtcAlarmStateSetStatusAddress;
+    uint8_t buffer;
+    buffer = 0x0fu;
+    /* Write the address of the status register */
+    if (!twi_send_sync(0xd0u, 1, &buffer)) {
+      /* Cannot write, try next tick */
+      return;
+    }
+    /* Read the status */
+    if (!twi_recv_sync(0xd0u, 1, &buffer)) {
+      /* Cannot read, try next tick */
+      return;
+    }
+    /* Check/handle the status */
+    if (!rtc_alarm_check_status(buffer)) {
+      /* Cannot handle status, try next tick */
+      return;
+    }
+
+    /* Now, we are ready to clear the ISR flag */
     TheIsrRequest = false;
   }
 }
 
-void rtc_alarm_twi_write_done(bool success)
-{
-  if (!success) {
-    TheState = RtcAlarmStateIdle;
-    mprintstrln(PSTR("Error: failed clearing alarm flag in RTC."));
-    return;
-  }
-
-  switch (TheState) {
-  case RtcAlarmStateSetStatusAddress:
-    twi_recv(0xd0, 1, rtc_alarm_read_ready);
-    TheState = RtcAlarmStateReadStatus;
-    break;
-  case RtcAlarmStateClearFlag:
-    /* Enable INT1 interrupt again */
-    GICR |= (1U<<INT1);
-    break;
-  default:
-    mprintstrln(PSTR("Error: wrong state in alarm management."));
-    break;
-  }
-}
-
-void rtc_alarm_read_ready(bool success, uint8_t length, const uint8_t *data)
-{
-  if (!success) {
-    TheState = RtcAlarmStateIdle;
-    mprintstrln(PSTR("Error: failed reading TWI for RTC alarm management."));
-    return;
-  }
-
-  switch (TheState) {
-  case RtcAlarmStateReadStatus:
-    if (1 == length) {
-      rtc_alarm_check_status(*data);
-    } else {
-      mprintstrln(PSTR("Error: wrong read package."));
-      TheState = RtcAlarmStateIdle;
-    }
-    break;
-  default:
-    mprintstrln(PSTR("Error: wrong state in alarm management."));
-    TheState = RtcAlarmStateIdle;
-    break;
-  }
-}
-
-void rtc_alarm_check_status(uint8_t status)
+bool rtc_alarm_check_status(uint8_t status)
 {
   /* First, check the alarm-1 */
   if (status & 0x01u) {
     mprintstrln(PSTR("Alarm1: triggered."));
-    cmd_engine_start();
 #ifdef MCODE_SOUND
     sound_play_note(0x49u, 500);
     sound_play_note(0x45u, 500);
@@ -147,16 +102,25 @@ void rtc_alarm_check_status(uint8_t status)
   }
 
   /* now, we are ready to clear the alarm(s) flags */
-  TheState = RtcAlarmStateClearFlag;
-  TheBuffer[0] = 0x0f;
-  TheBuffer[1] = 0x88;
-  twi_send(0xd0, 2, TheBuffer, rtc_alarm_twi_write_done);
+  if (0u != (status & 0x03u)) {
+    uint8_t buffer[2];
+    buffer[0] = 0x0f;
+    buffer[1] = 0x88;
+    if (!twi_send_sync(0xd0u, 2, buffer)) {
+      /* Cannot clear the status */
+      return false;
+    }
+  }
+
+  /* Enable INT1 interrupt again */
+  GICR |= _BV(INT1);
+  return true;
 }
 
 ISR(INT1_vect)
 {
   TheIsrRequest = true;
   /* Disable the INT1 interrupt */
-  GICR &= ~(1U<<INT1);
-  GIFR |= (1U<<INTF1);
+  GICR &= ~_BV(INT1);
+  GIFR |= _BV(INTF1);
 }
